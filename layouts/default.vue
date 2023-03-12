43b50ed991f7e9d5bad3a5a47cf631e970c322be
JacobGlitcher <hakob.yer@gmail.com>
2023-03-12 18:32:20 +0400

    creating route's overall structure

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Buysell Nuxt</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+    <div>
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,15 @@
 <template>
-  <p>products</p>
+  <div>
+    <p>Product details for {{ id }}</p>
+  </div>
 </template>
 
 <script setup>
   definePageMeta({
     layout: "products-layout",
   })
+
+  const { id } = useRoute().params
 </script>
 
 <style lang="scss" scoped>
